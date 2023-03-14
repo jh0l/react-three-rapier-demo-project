7ccd241ce2ff53ecfea47ab0fbfe6e0552d9f853
JackHollisLondon(s3585034) <me@jh0l.com>
2023-03-15 01:41:56 +1100

    feat: :recycle: refactor control code
    create route for robot to go around track and back to start

--- src/examples/car/utils/useControls.tsx
+++ src/examples/car/utils/useControls.tsx
@@ -3,41 +3,80 @@ import { CanvasRes } from "./useCanvasMap";
 import { useFrame } from "@react-three/fiber";
 import { useDemo } from "../../../App";
 import { mapLinear } from "three/src/math/MathUtils";
+export interface ControlRes {
+    left: number;
+    right: number;
+    auto: boolean;
+    commands: {
+        queue: [CommandMaker, TriggerMaker][];
+        command: Command;
+    };
+    sample: boolean;
+}
 
 export function useControls(canvasRef: RefObject<CanvasRes>) {
-    const idx = useRef({ v: 0 });
+    const idx = useRef({ v: -1 });
     const ref = useRef<ControlRes>({
         left: 0,
         right: 0,
         auto: false,
         commands: {
             queue: [
-                go(canvasRef, () => idx.current.v++, timer(300)),
-                trace(canvasRef, () => idx.current.v++, intersection),
-                stop(canvasRef, () => idx.current.v++, timer(10)),
-                align(canvasRef, () => idx.current.v++, timer(500)),
-                drive(canvasRef, () => idx.current.v++, timer(350), -1),
-                trace(canvasRef, () => idx.current.v++, intersection),
-                drive(canvasRef, () => idx.current.v++, timer(350), 0),
-                trace(canvasRef, () => idx.current.v++, intersection),
-                // drive(canvasRef, () => idx.current.v++, timer(1000), 0),
-                fin(canvasRef, () => idx.current.v++, none),
+                [go, timer(300)],
+                [trace, intersection("R")],
+                [stop, timer(10)],
+                [align, timer(500)],
+                [drive(-1), timer(350)],
+                [trace, intersection("R")],
+                [stop, timer(10)],
+                [trace, intersection("T")],
+                [drive(1), timer(800)],
+                [align, timer(500)],
+                [drive(0), timer(100)],
+                [trace, intersection("L")],
+                [stop, timer(10)],
+                [trace, intersection("T")],
+                [stop, timer(10)],
+                [align, timer(500)],
+                [drive(1), timer(350)],
+                [trace, timer(2100)],
+                [drive(1), timer(790)],
+                [fin, none],
             ],
+            command: () => [0, 0],
         },
         sample: false,
     });
     useKeyboard(ref, idx);
+    function nextFn() {
+        if (!canvasRef.current) return;
+        ref.current.commands.command = ref.current.commands.queue[
+            idx.current.v
+        ][0](
+            ref.current.commands.queue[idx.current.v][1](
+                canvasRef.current,
+                nextFn
+            ),
+            canvasRef
+        );
+        idx.current.v += 1;
+    }
     useFrame(() => {
         const { commands, auto, sample } = ref.current;
-        if (auto) {
+        if (auto && canvasRef.current) {
+            if (idx.current.v < 0 && commands.queue.length) {
+                idx.current.v = 0;
+                nextFn();
+            }
             if (idx.current.v < commands.queue.length) {
-                let [left, right] = commands.queue[idx.current.v]().map(
-                    (x) =>
-                        Math.max(-1, Math.min(1, Math.trunc(x * 100) / 100)) ||
-                        0
-                );
-                ref.current.left = left;
-                ref.current.right = right;
+                const output = commands.command();
+                for (let i = 0, x; i < output.length; i++) {
+                    x = output[i];
+                    x = Math.max(-1, Math.min(1, Math.trunc(x * 100) / 100));
+                    output[i] = x || 0;
+                }
+                ref.current.left = output[0];
+                ref.current.right = output[1];
             } else {
                 ref.current.auto = false;
             }
@@ -79,15 +118,7 @@ const keyMap: KeyMapType<string> = {
     r: "reset",
     p: "paused",
 };
-export interface ControlRes {
-    left: number;
-    right: number;
-    auto: boolean;
-    commands: {
-        queue: Command[];
-    };
-    sample: boolean;
-}
+
 function useKeyboard(
     res: RefObject<ControlRes>,
     idx: RefObject<{ v: number }>
@@ -148,7 +179,7 @@ function useKeyboard(
             }
             if (auto) {
                 res.current.auto = !res.current.auto;
-                idx.current.v = 0;
+                idx.current.v = -1;
             }
             res.current.sample = sample;
             if (reset && demo.resetPhysics && demo.setPaused) {
@@ -163,11 +194,11 @@ function useKeyboard(
     });
 }
 
-type CommandMaker<P = void> = (
-    can: RefObject<CanvasRes>,
-    next: () => void,
-    nextTrigger: (can: CanvasRes) => boolean,
-    param?: P
+type CommandMakerMaker<P> = (p: P) => CommandMaker;
+
+type CommandMaker = (
+    nextTrigger: () => boolean,
+    can: RefObject<CanvasRes>
 ) => Command;
 
 type Command = () => [number, number];
@@ -179,34 +210,47 @@ const quadraticMap: mapLog = (x, a1, a2, b1, b2) => {
     return mapLinear(x, a1, a2, b1, b2);
 };
 
-type Trigger = (can?: CanvasRes) => boolean;
-const none = () => false;
+type Trigger = () => boolean;
+type TriggerMaker = (can: CanvasRes, next: () => void) => Trigger;
+type TriggerMakerMaker<T> = (p: T) => TriggerMaker;
 
-const intersection: Trigger = (can?: CanvasRes) => {
-    if (!can) return false;
-    const { top, bot, rgt } = can.luminance.keys();
-    return [top, bot, rgt].every((x) => x < 0.45);
-};
+const none: TriggerMaker = () => () => false;
 
 // TODO use TriggerMaker to customize intersection to use different intersection types
+const intersection: TriggerMakerMaker<"R" | "L" | "T"> =
+    (lock) => (can: CanvasRes, next) => {
+        return () => {
+            if (!can) return false;
+            const { top, bot, rgt, lft } = can.luminance.keys();
+            const arr =
+                lock === "R"
+                    ? [rgt, top, bot]
+                    : lock === "L"
+                    ? [lft, top, bot]
+                    : [bot, rgt, lft];
+            const res = arr.every((x) => x < 0.45);
+            if (res) {
+                next();
+                return true;
+            }
+            return false;
+        };
+    };
 
-const go: CommandMaker = (canRef, next, trigger) => {
+const go: CommandMaker = (trigger) => {
     return () => {
-        if (!canRef.current) return [0, 0];
-        const can = canRef.current;
-        if (trigger(can)) {
-            next();
+        if (trigger()) {
             return [0, 0];
         }
         return [1, 1];
     };
 };
-const trace: CommandMaker = (canRef, next, trigger) => {
+
+const trace: CommandMaker = (trigger, canRef) => {
     return () => {
         if (!canRef.current) return [0, 0];
         const can = canRef.current;
-        if (trigger(can)) {
-            next();
+        if (trigger()) {
             return [0, 0];
         }
         let LEFT = 1,
@@ -226,7 +270,7 @@ const trace: CommandMaker = (canRef, next, trigger) => {
 
 // turn the wheels in opposing directions by ratio until left and right sensors reach
 // equalibrium
-const align: CommandMaker = (canRef, next, trigger) => {
+const align: CommandMaker = (trigger, canRef) => {
     return () => {
         if (!canRef.current) return [0, 0];
         const can = canRef.current;
@@ -240,36 +284,37 @@ const align: CommandMaker = (canRef, next, trigger) => {
         } else if (rgt > lft) {
             LEFT = -turn;
         }
-        if (trigger(can) && turn < 0.1) next();
+        if (turn < 0.1) trigger();
         return [LEFT, RIGHT];
     };
 };
 
-type TriggerMaker<T> = (p: T) => Trigger;
-const timer: TriggerMaker<number> = (ms: number) => {
-    const state = { started: false, time: Infinity };
-    return (_?: CanvasRes) => {
-        if (!state.started) {
-            state.started = true;
-            state.time = Date.now() + ms;
-        } else if (state.time < Date.now()) {
-            return true;
-        }
-        return false;
+const timer: TriggerMakerMaker<number> = (ms) => {
+    return (_, next) => {
+        const state = { started: false, time: Infinity };
+        return () => {
+            if (!state.started) {
+                state.started = true;
+                state.time = Date.now() + ms;
+            } else if (state.time < Date.now()) {
+                next();
+                return true;
+            }
+            return false;
+        };
     };
 };
 
-const stop: CommandMaker = (canRef, next, trigger) => {
+const stop: CommandMaker = (trigger) => {
     return () => {
-        if (!canRef.current) return [0, 0];
-        if (trigger(canRef.current)) next();
+        trigger();
         return [0, 0];
     };
 };
 
-const fin: CommandMaker = (_, next) => {
+const fin: CommandMaker = (trigger) => {
     return () => {
-        next();
+        trigger();
         console.log("DONE");
         return [0, 0];
     };
@@ -286,17 +331,10 @@ const fin: CommandMaker = (_, next) => {
 
 // drive the wheels with a degree of turning: 0 = straight, -1 = full turn left, 1 = full turn right
 type DriveParam = number;
-const drive: CommandMaker<DriveParam> = (
-    canRef,
-    next,
-    trigger,
-    degrees = 0
-) => {
+const drive: CommandMakerMaker<DriveParam> = (degrees) => (trigger, canRef) => {
     return () => {
         if (!canRef.current) return [0, 0];
-        const can = canRef.current;
-        if (trigger(can)) {
-            next();
+        if (trigger()) {
             return [0, 0];
         }
         let LEFT = 1,
