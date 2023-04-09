7401012cfc385b5e4c9986af1bf39d3f227edd22
JackHollisLondon(s3585034) <me@jh0l.com>
2023-04-09 15:59:02 +1000

    feat: :sparkles: Add PushyBoy, UnitStack, Car components to Level

--- src/pages/car/Level.tsx
+++ src/pages/car/Level.tsx
@@ -1,9 +1,22 @@
 import { Image, Box, Cylinder } from "@react-three/drei";
 import { MAP_ASP, MAP_SCALE } from "./utils/useCanvasMap";
 import { CarEntity } from "./CarEntity";
-import { MeshStandardMaterial } from "three";
-import { RigidBody } from "@react-three/rapier";
+import {
+    MeshPhysicalMaterial,
+    MeshStandardMaterial,
+    TorusGeometry,
+} from "three";
+import {
+    CuboidCollider,
+    RapierRigidBody,
+    RigidBody,
+    TrimeshCollider,
+} from "@react-three/rapier";
 import { useJitRef } from "../../utils";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
 export default function Level() {
     return (
@@ -22,124 +35,208 @@ function FuelStation() {
     const mat = useJitRef(
         () =>
             new MeshStandardMaterial({
-                color: "yellow",
+                color: "rgb(190, 150, 0)",
+                // opacity: 0.9,
+                // transparent: true,
+            })
+    );
+    const grayMat = useJitRef(
+        () =>
+            new MeshStandardMaterial({
+                color: "gray",
                 opacity: 0.9,
                 transparent: true,
             })
     );
     return (
-        <>
+        <group position={[-43.24, -5, -11.94]}>
             {/* MAIN BOX */}
-            <RigidBody>
+            <RigidBody lockTranslations={true}>
                 <Box
                     scale={[4.2, 5, 5.74]}
-                    position={[-43.24, -5, -11.94]}
                     material={mat.current}
+                    receiveShadow
                 />
                 {/* BRIDGE */}
-                <Box scale={[3.5, 0.5, 0.5]} position={[-39.5, -5, -13.4]}>
+                <Box scale={[3.5, 0.5, 0.5]} position={[3.74, 0, -1.46]}>
                     <meshStandardMaterial color="black" />
                 </Box>
-                <Box scale={[3.5, 0.5, 0.5]} position={[-39.5, -6, -13.4]}>
+                <Box scale={[3.5, 0.5, 0.5]} position={[3.74, -1, -1.46]}>
                     <meshStandardMaterial color="black" />
                 </Box>
                 {/* FRONT BOX */}
                 <Box
                     scale={[2.7, 3.5, 4.1]}
-                    position={[-36.85, -5.75, -13.4]}
+                    position={[6.39, -0.75, -1.46]}
                     material={mat.current}
                 />
                 {/* RESTBOX */}
-                <Box scale={[0.5, 0.5, 1.12]} position={[-35, -7.1, -13.4]}>
+                <Box scale={[0.5, 0.5, 1.12]} position={[8.24, -1.8, -1.46]}>
+                    <meshStandardMaterial color="orange" />
+                </Box>
+                {/* TOPBOX (limits PUSHY BOY) */}
+                <Box scale={[0.5, 0.5, 1.12]} position={[8.24, 0.7, -1.46]}>
                     <meshStandardMaterial color="orange" />
                 </Box>
             </RigidBody>
             {/* PUSHY BOY */}
-            <RigidBody
-                enabledTranslations={[false, true, false]}
-                lockRotations={true}
-            >
-                <Box scale={[0.5, 0.5, 4.1]} position={[-35, -1.1, -13.4]}>
-                    <meshStandardMaterial color="gray" />
-                </Box>
-            </RigidBody>
-            {/* CAR */}
-            <RigidBody>
-                <Box
-                    scale={[2, 2, 4]}
-                    position={[-43.96, -5.6, -6.7]}
-                    material={mat.current}
-                />
-                <Cylinder
-                    args={[0.45, 0.45, 0.45, 16]}
-                    rotation={[0, 0, Math.PI / 2]}
-                    position={[-42.7, -6.5, -5.45]}
-                >
-                    <meshStandardMaterial
-                        color="gray"
-                        transparent={true}
-                        opacity={0.8}
-                    />
-                </Cylinder>
-                <Cylinder
-                    args={[0.45, 0.45, 0.45, 16]}
-                    rotation={[0, 0, Math.PI / 2]}
-                    position={[-42.7, -6.5, -6.75]}
-                >
-                    <meshStandardMaterial
-                        color="gray"
-                        transparent={true}
-                        opacity={0.8}
-                    />
-                </Cylinder>
-                <Cylinder
-                    args={[0.45, 0.45, 0.45, 16]}
-                    rotation={[0, 0, Math.PI / 2]}
-                    position={[-45.21, -6.5, -8.1]}
-                >
-                    <meshStandardMaterial
-                        color="gray"
-                        transparent={true}
-                        opacity={0.8}
-                    />
-                </Cylinder>
-                <Cylinder
-                    args={[0.45, 0.45, 0.45, 16]}
-                    rotation={[0, 0, Math.PI / 2]}
-                    position={[-45.21, -6.5, -5.45]}
-                >
-                    <meshStandardMaterial
-                        color="gray"
-                        transparent={true}
-                        opacity={0.8}
-                    />
-                </Cylinder>
-                <Cylinder
-                    args={[0.45, 0.45, 0.45, 16]}
-                    rotation={[0, 0, Math.PI / 2]}
-                    position={[-45.21, -6.5, -6.75]}
-                >
-                    <meshStandardMaterial
-                        color="gray"
-                        transparent={true}
-                        opacity={0.8}
-                    />
-                </Cylinder>
-                <Cylinder
-                    args={[0.45, 0.45, 0.45, 16]}
-                    rotation={[0, 0, Math.PI / 2]}
-                    position={[-42.7, -6.5, -8.1]}
-                >
-                    <meshStandardMaterial
-                        color="gray"
-                        transparent={true}
-                        opacity={0.8}
-                    />
-                </Cylinder>
-            </RigidBody>
-        </>
+            <PushyBoy mat={grayMat} />
+            <Car mat={mat} grayMat={grayMat} />
+            <UnitStack />
+        </group>
     );
 }
+
+const UnitStack = () => {
+    const { stored, units } = useFuelStationStore();
+    const mat = useJitRef(
+        () =>
+            new MeshPhysicalMaterial({
+                color: "hotpink",
+                // @ts-ignore
+                iridescence: 1,
+                iridescenceIOR: 1,
+                // iridescenceThicknessRange={[0, 1400]}
+                roughness: 1,
+                clearcoat: 0.5,
+                metalness: 0.75,
+            })
+    );
+    return (
+        <>
+            {Array(stored)
+                .fill(0)
+                .map((_, i) => (
+                    <Cylinder
+                        args={[2 / 3, 2 / 3, 1, 10]}
+                        castShadow
+                        position={[0, 3.1 + i * 1.07, 0]}
+                        material={mat.current}
+                        key={i + 1}
+                    />
+                ))}
+            {units.map((id) => (
+                <RigidBody>
+                    <Cylinder
+                        args={[2 / 3, 2 / 3, 1, 10]}
+                        position={[-0.75, 3.1, 5]}
+                        material={mat.current}
+                        key={id}
+                    />
+                </RigidBody>
+            ))}
+        </>
+    );
+};
+
+const WHEEL_POS: [number, number, number][] = [
+    [1.26, -0.9, 1.25],
+    [1.26, -0.9, -0.05],
+    [-1.25, -0.9, -1.4],
+    [-1.25, -0.9, 1.25],
+    [-1.25, -0.9, -0.05],
+    [1.26, -0.9, -1.4],
+];
+
+const WHEEL_ARGS: [number, number, number, number] = [0.45, 0.45, 0.45, 16];
+
+const WHEEL_ROT: [number, number, number] = [0, 0, Math.PI / 2];
+
+type MatRef = { current: MeshStandardMaterial };
+
+const Car = ({ mat, grayMat }: { mat: MatRef; grayMat: MatRef }) => {
+    const torus = useJitRef(() => new TorusGeometry(1.2, 0.3, 4, 4));
+    console.log(torus);
+    return (
+        <group position={[-0.72, 1, 5.9]} scale={1.2}>
+            <RigidBody colliders={false}>
+                <CuboidCollider
+                    args={[2, 2, 3]}
+                    scale={0.5}
+                    position={[0, 0, -0.81]}
+                />
+                <Box
+                    scale={[2, 2, 3]}
+                    position={[0, 0, -0.81]}
+                    material={mat.current}
+                />
+                <TrimeshCollider
+                    args={[
+                        // @ts-ignore
+                        torus.current.attributes.position.array,
+                        // @ts-ignore
+                        torus.current.index.array,
+                    ]}
+                    position={[0, 0, 1.56]}
+                    rotation={[-Math.PI / 3.2, 0, Math.PI / 4]}
+                />
+                <mesh
+                    geometry={torus.current}
+                    material={mat.current}
+                    position={[0, 0, 1.56]}
+                    rotation={[-Math.PI / 3.2, 0, Math.PI / 4]}
+                />
+                {WHEEL_POS.map((pos, i) => (
+                    <Cylinder
+                        args={WHEEL_ARGS}
+                        rotation={WHEEL_ROT}
+                        position={pos}
+                        material={grayMat.current}
+                        key={i + 1}
+                    />
+                ))}
+                <CuboidCollider
+                    args={[0.45, 0.9, 3.55]}
+                    scale={0.5}
+                    position={[1.25, -0.9, -0.07]}
+                />
+                <CuboidCollider
+                    args={[0.45, 0.9, 3.55]}
+                    scale={0.5}
+                    position={[-1.25, -0.9, -0.07]}
+                />
+            </RigidBody>
+        </group>
+    );
+};
+
+interface FuelStationState {
+    isDropped: boolean;
+    isLifted: boolean;
+    stored: number;
+    // array of IDs for units that have been spawned
+    units: string[];
+}
+
+export const useFuelStationStore = create<FuelStationState>()(
+    devtools((set) => ({
+        isDropped: false,
+        isLifted: false,
+        stored: 5,
+        units: ["bing"],
+        setDropped: (v: boolean) => set({ isDropped: v }),
+        setLifted: (v: boolean) => set({ isLifted: v }),
+        setStored: (v: number) => set({ stored: v }),
+    }))
+);
+
+const PushyBoy = ({ mat }: { mat: MatRef }) => {
+    const ref = useRef<RapierRigidBody>(null);
+    useFrame(() => {});
+    return (
+        <RigidBody
+            enabledTranslations={[false, true, false]}
+            lockRotations={true}
+            ref={ref}
+        >
+            <Box
+                scale={[0.5, 0.5, 4.1]}
+                position={[8.24, -0.6, -1.46]}
+                material={mat.current}
+            ></Box>
+        </RigidBody>
+    );
+};
 
 const Map = ({ map_url }: { map_url: string }) => {
     return (
